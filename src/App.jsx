0130c9a314455c2055b986b95261cec58e4a33b2
ArriagaAndre94 <arriaga1094@hotmail.com>
2025-02-12 17:44:28 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import Input from './components/Input'
 function App() {
   return (
     <Formik>
-      <Input name='accion' label='Ingrese una acción'/>
+      <form>
+        <Input name='accion' label='Ingrese una acción' />
+        <Input name='carrera' label='Ingrese una carrera' />
+      </form>
     </Formik>
   )
 }
